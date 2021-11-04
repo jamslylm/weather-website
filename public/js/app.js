ae53f22964cf1f439933f05f9174a2491df4516e
Jamsly-love MINÉ <mjamsly98@gmail.com>
2021-11-04 16:15:10 -0400

    Add more forecasts data and set dynamic weather icon

--- public/js/app.js
+++ public/js/app.js
@@ -2,6 +2,7 @@ const weatherForm = document.querySelector('form')
 const searchElement = document.querySelector('input')
 const messageOne = document.querySelector('#message-1')
 const messageTwo = document.querySelector('#message-2')
+const weatherImg = document.querySelector('.weather-img')
 
 weatherForm.addEventListener('submit', (e) => {
     e.preventDefault()
@@ -14,9 +15,11 @@ weatherForm.addEventListener('submit', (e) => {
         response.json().then((data) => {
             if (data.error) {
                 messageOne.textContent = data.error
+                weatherImg.setAttribute('alt', '😥')
             } else {
                 messageOne.textContent = data.location
                 messageTwo.textContent = data.forecast.weatherDesc + ', it\'s ' + data.forecast.temperature + ' degrees out but it\'s feels like ' + data.forecast.feelsLike + ' degrees out'
+                weatherImg.setAttribute('src', data.forecast.weatherIcon[0])
             }
         })
     })
